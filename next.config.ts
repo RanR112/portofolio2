0d93ee95973ae6ae598ba2130be4620079e63f07
RanR112 <146650544+RanR112@users.noreply.github.com>
2026-03-07 23:28:51 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = { devIndicators: false };
+const nextConfig: NextConfig = {
+    devIndicators: false,
+};
 
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 export default withNextIntl(nextConfig);
